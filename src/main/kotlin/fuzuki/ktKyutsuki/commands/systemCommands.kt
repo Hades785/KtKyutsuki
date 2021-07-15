23f9966607555752f9354957c029147f8c1a5ef2
フズキ <hades785@gmail.com>
2021-07-15 20:06:36 +0200

    [commands] Help command lists all registered commands' names.

--- src/main/kotlin/fuzuki/ktKyutsuki/commands/systemCommands.kt
+++ src/main/kotlin/fuzuki/ktKyutsuki/commands/systemCommands.kt
@@ -1,9 +1,15 @@
 package fuzuki.ktKyutsuki.commands
 
 import fuzuki.ktKyutsuki.core.commands.Command
+import fuzuki.ktKyutsuki.core.commands.CommandRegistry
 
 val help = Command("help") {
-    channel.createMessage("TEST")
+    val commands = CommandRegistry.getAll()
+
+    val msg = StringBuilder()
+    for (command in commands) msg.append(command.name).append("\n")
+
+    channel.createMessage(msg.toString())
 }
 
 val systemCommands = listOf(help)
