f32bbdbaff5e057c380990757a1554647b0d39a9
フズキ <hades785@gmail.com>
2021-07-24 00:58:50 +0200

    [core/commands] Add function to get command arguments from message content.

--- src/main/kotlin/fuzuki/ktKyutsuki/core/commands/Command.kt
+++ src/main/kotlin/fuzuki/ktKyutsuki/core/commands/Command.kt
@@ -3,8 +3,17 @@ package fuzuki.ktKyutsuki.core.commands
 import dev.kord.core.entity.Message
 import fuzuki.ktKyutsuki.core.logging.LogLevel
 import fuzuki.ktKyutsuki.core.logging.Logger
+import fuzuki.ktKyutsuki.env
 
 class Command(val name: String, val description: String, private val run: suspend Message.() -> Unit) {
+    companion object {
+        fun getCommandArguments(message: Message): List<String> {
+            val noPrefix = message.content.split(env["PREFIX"])[1]
+            val split = noPrefix.split(" ")
+            return split.subList(1, split.count())
+        }
+    }
+
     suspend fun execute(message: Message) {
         Logger.log(LogLevel.Info, "Command \"$name\" was called.")
         try {
